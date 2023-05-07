2f53dd3ebc88906e4eeac3f79481eb17274c6b0f
owen8877 <9379520+owen8877@users.noreply.github.com>
2023-05-07 14:17:34 -0500

    Hotfix for pm2 pid. (Again x2?)

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     name: 'calendar-as-diary-py',
     cmd: 'main.py',
     autorestart: false,
-    pid_file: '/home/webhookd/.pm2/pids/calendar-as-diary-py-0.pid'
+    pid_file: '/home/webhookd/.pm2/pids/calendar-as-diary-py-0.pid',
     watch: false,
     interpreter: 'python',
   }],
